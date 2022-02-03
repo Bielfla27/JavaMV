50480abab20d5a10a79a698bcf97b7ba1d4ed7af
Bielfla27 <gabibielmft@outlook.com>
2022-02-03 16:46:37 -0300

    curso java mv

---
+++ Atividade_24_01_2022/ProjetoJavaHelloWord/src/br/com/aula/java/hellow_word.java
@@ -0,0 +1,15 @@
+package br.com.aula.java;
+
+public class hellow_word {
+	
+	private static String mensagem; 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		mensagem = "olá mundo java";
+				
+		System.out.println(mensagem);
+	}
+
+}
